bd79991dfa959976f71db0ccbae899af0080e198
youssefboussaid <youssbsd@gmail.com>
2021-10-25 12:51:42 -1100

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 listint_t *current, *swap, *tmp;
 
-if (!*list)
+if (list == NULL || *list == NULL)
 	return;
 current = *list;
 while ((current = current->next))
