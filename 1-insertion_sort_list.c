f61efe372c5edf6b94623361dd211c718ca52be7
youssefboussaid <youssbsd@gmail.com>
2021-10-25 12:50:05 -1100

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,37 +1,35 @@
 #include "sort.h"
 
 /**
- *bubble_sort - sorts an array with the bubble methode
+ *insertion_sort_list - sorts an array with the insertion sort methode
  *@list: double linked list
  *Return: void
  */
 
-void insertion_sort_list(listint_t **list);
+void insertion_sort_list(listint_t **list)
 {
-
 listint_t *current, *swap, *tmp;
 
-	if (!*list)
-		return;
-	current = *list;
-	while ((current = current->next))
+if (!*list)
+	return;
+current = *list;
+while ((current = current->next))
+{
+	swap = current;
+	while (swap->prev && swap->n < swap->prev->n)
 	{
-		swap = current;
-		while (swap->prev && swap->n < swap->prev->n)
-		{
-				tmp = swap->prev;
-				if (swap->next)
-					swap->next->prev = tmp;
-				if (tmp->prev)
-					tmp->prev->next = swap;
-				else
-				 		*list = swap;
-			tmp->next = swap->next;
-			swap->prev = tmp->prev;
-			swap->next = tmp;
-			tmp->prev = swap;
-			print_list(*list);
-		}
-		
+		tmp = swap->prev;
+		if (swap->next)
+			swap->next->prev = tmp;
+		if (tmp->prev)
+			tmp->prev->next = swap;
+		else
+			*list = swap;
+		tmp->next = swap->next;
+		swap->prev = tmp->prev;
+		swap->next = tmp;
+		tmp->prev = swap;
+		print_list(*list);
 	}
+}
 }
